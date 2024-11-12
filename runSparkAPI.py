843468543d226f83a6c56d8701de4d6d9be45ea0
lin15266115 <60529381+lin15266115@users.noreply.github.com>
2024-11-12 17:12:30 +0800

    Add files via upload
    星火消息.调用的ai版本()更新:对应的版本编号改为5 -> Spark Max-32K; 6 -> Spark4.0 Ultra
    优化SparkAPI.gen_params()

--- runSparkAPI.py
+++ runSparkAPI.py
@@ -1,12 +1,16 @@
-# 1.2
-# import time
-#以下密钥信息从控制台获取   https://console.xfyun.cn/services/bm35
+# 1.2.1
+# 使用python3.12编写
 
 class 认证信息:
     """
     你需要注册讯飞星火的账号,购买讯飞星火产品,\n
-    并从https://console.xfyun.cn/services/cbm 
+    并从https://console.xfyun.cn/services/cbm
     获取你的appid, api_secret, api_key并在创建实例时填写进来\n
+    
+    Parameters:
+        APPID (str):控制台中的 appid
+        APISecret (str):控制台中的 api_secret
+        APIKey (str):控制台中的 api_key
     """
     def __init__(
              self
@@ -26,22 +30,29 @@ class 星火消息:
             ,最大字符长度 = 8000
         ):
         """
-        初始化星火消息
-        最大字符长度 -> 请在官方文档中查看你使用的版本支持的最大token数来修改这个\n
-        你需要先创建一个 **认证信息** 实例, 再创建一个星火消息\n
-        星火消息在调用SparkApi时,默认情况下将会使用Spark Lite版本\n
-        .\n
-        ### **下面是星火消息提供的函数**\n
-        **更改ai设定** -> 修改ai的设定\n
-        **置最大字符长度** -> 所有消息加起来的字符总数将不超过此数字, 默认8000\n
-        **置消息历史** -> 插入历史消息,role为该消息发送的对象, 通常可以不使用此项而是使用另外两项\n
-        **获取回复** -> 调用官方文档向ai发送星火消息的内容并获取回复\n
-        **置用户消息到历史** -> 向星火消息内加入用户的消息, 最新一次添加的消息将被判定为ai要回复的内容\n
-        **置入ai回复到历史** -> 目前在默认情况下会自动置入ai回复\n
-        **取历史消息总长度** -> 取所有历史消息的总字符数\n
-        **检查消息长度并处理** -> 如果超出了 *最大字符长度* 的限制, 它将删除最早的历史消息,如果仍然超出,它将删除设定.建议开发者调用时不要允许用户发送超过(最大字符长度-设定文本的长度)\n
-        **调用的ai版本** -> 用于更改调用的讯飞星火大模型版本, 已经加入了文档中提供的所有大模型版本\n
-        **自动置历史回复** -> 开关自动置入ai回复的函数, 如果关闭该功能, ai将不会参考历史中它自己的回复 **(该功能被设计为关闭历史消息选项, 但是并未完善, 不建议关闭该功能)**
+        星火消息类，用于与讯飞星火API进行交互。
+        
+        Attributes:
+            设定 (dict): 系统角色和内容设定。
+            历史 (list): 存储对话历史的消息列表。
+            新消息 (str): 待发送的新消息。
+            appid (str): 控制台中获取的APPID信息。
+            api_secret (str): 控制台中获取的APISecret信息。
+            api_key (str): 控制台中获取的APIKey信息。
+            最大字符长度 (int): 允许的最大字符长度。
+            自动置历史 (bool): 是否自动将用户消息和AI回复添加到历史记录中。
+        
+        Methods:
+            更改ai设定(内容： str): 设置讯飞星火的设定，填入一段文本作为设定内容。
+            置最大字符长度(长度： int): 设置允许的最大字符长度。
+            置消息历史(role, content: str): 向历史记录中添加一条消息。
+            获取回复(使用历史回复=True): 调用内部main函数获取回复，并根据需要更新历史记录。
+            置用户消息到历史(text: str): 将用户消息添加到历史记录中。
+            置AI回复到历史(text: str): 将AI回复添加到历史记录中。
+            取历史消息总长度(): 计算历史记录中所有消息的总字符长度。
+            检查消息长度并处理(): 如果消息长度超过最大字符长度，则删除最早的历史记录直到满足条件。
+            调用的ai版本(版本： int): 选择使用的讯飞星火API版本。
+            自动置历史回复(开关： bool): 设置是否自动将用户消息和AI回复添加到历史记录中。
         """
         self.设定 = {"role": "system", "content": ""}
         self.历史 = []
@@ -74,14 +85,14 @@ class 星火消息:
         总消息 = [self.设定]
         if 使用历史回复 :
             总消息 += self.历史
-            # self.检查消息长度并处理()
+            self.检查消息长度并处理()
 
         新消息 = {"role": "user", "content": self.新消息}
         总消息.append(新消息)
 
         # print(self.消息)
-        self.api.answer = ""
-        self.api.main(总消息)
+        self.api.answer = "" # 重置回复信息
+        self.api.main(总消息)# 获取回复
 
         回复 = self.api.answer
 
@@ -115,13 +126,21 @@ class 星火消息:
             ,版本:int
         ):
         """
-        你可以选择以下的数字: 0,1,2,3,4,5\n
-        0 -> Spark Lite\n
-        1 -> Spark V2.0\n
-        2 -> Spark Pro \n
-        3 -> Spark Pro-128K\n
-        4 -> Spark Max \n
-        5 -> Spark4.0 Ultra \n
+        调用不同的AI版本。该函数接受一个整数参数，代表要调用的AI版本。
+        
+        Parameters:
+            version (int): 
+                可选的数字包括0,1,2,3,4,5，分别对应以下版本：\n
+                0 -> Spark Lite\n
+                1 -> Spark V2.0\n
+                2 -> Spark Pro\n
+                3 -> Spark Pro-128K\n
+                4 -> Spark Max\n
+                5 -> Spark4.0 Ultra\n
+        
+        Returns:
+            None:
+            无返回值。但会更改对象的Spark_url和domain属性，以使用相应的AI版本。
         """
         所有版本 = (
             ['wss://spark-api.xf-yun.com/v1.1/chat','general'],       # 0
@@ -138,7 +157,7 @@ class 星火消息:
     def 自动置历史回复(self,开关:bool):
             self.自动置历史 = 开关
 
-
+# 以下代码修改自讯飞星火官方文档
 import _thread as thread
 import base64
 import datetime
@@ -156,7 +175,32 @@ import websocket  # 使用websocket_client
 
 
 class SparkAPI():
-    def __init__(self, 认证信息:认证信息, Spark_url, domain):
+    """
+    SparkAPI类用于与Spark的WebSocket API进行交互。它提供了初始化连接、发送问题、接收回答等功能。通常可以不使用SparkAPI类
+    
+    Parameters:
+        认证信息 (object): 包含appid等信息的认证对象。
+        Spark_url (str): Spark WebSocket API的URL。
+        domain (str): 指定大模型版本，例如'generalv2.5'。
+    
+    Attributes:
+        answer (str): 存储从Spark API接收到的回答。
+        sid (str): 会话ID，用于跟踪特定的对话。
+        ws参数 (object): 包含WebSocket连接所需的参数的对象。
+        ws (object): WebSocketApp实例，用于建立和管理WebSocket连接。
+        appid (str): 应用程序的ID，用于认证和识别。
+        domain (str): 大模型版本。
+    
+    Methods:
+        on_error(ws, error): 当WebSocket发生错误时调用的处理函数。
+        on_close(ws, one, two): 当WebSocket关闭时调用的处理函数。
+        on_open(ws): 当WebSocket连接打开时调用的处理函数。
+        run(*args): 在新线程中运行，向WebSocket发送请求并处理响应。
+        on_message(ws, message): 当从WebSocket接收到消息时调用的处理函数。
+        gen_params(appid, domain, question): 根据appid和用户的提问生成请求参数。
+        main(question): 主函数，用于启动WebSocket连接并发送问题。
+    """
+    def __init__(self, 认证信息:认证信息, Spark_url, domain, max_tokens=4096):
         self.answer = ""
         self.sid = ''
         self.ws参数 = ws参数(认证信息, Spark_url)
@@ -164,6 +208,7 @@ class SparkAPI():
         self.ws = websocket.WebSocketApp("", on_message=self.on_message, on_error=self.on_error, on_close=self.on_close, on_open=self.on_open)
         self.appid = 认证信息.appid
         self.domain = domain
+        self.max_tokens = max_tokens
 
 
     # 收到websocket错误的处理
@@ -172,7 +217,7 @@ class SparkAPI():
     
     
     # 收到websocket关闭的处理
-    def on_close(self, ws, one,two):
+    def on_close(self, ws, one, two):
         pass
     
     
@@ -182,13 +227,7 @@ class SparkAPI():
     
     
     def run(self, *args):
-        data = json.dumps(
-            self.gen_params(
-                 appid=self.appid
-                ,domain= self.domain
-                ,question=self.question
-                )
-            )
+        data = json.dumps(self.gen_params())
         self.ws.send(data)
     
     
@@ -213,21 +252,21 @@ class SparkAPI():
                 self.ws.close()
     
     
-    def gen_params(self, appid, domain, question):
+    def gen_params(self):
         """
         通过appid和用户的提问来生成请参数
         """
         data = {
             "header": {
-                "app_id": appid,
+                "app_id": self.appid,
                 "uid": "1234"
             },
             "parameter": {
     
                 "chat": {
-                    "domain": domain,
+                    "domain": self.domain,
                     "temperature": 0.8,
-                    "max_tokens": 4096,
+                    "max_tokens": self.max_tokens,
                     "top_k": 5,
     
                     "auditing": "default"
@@ -235,7 +274,7 @@ class SparkAPI():
             },
             "payload": {
                 "message": {
-                    "text": question
+                    "text": self.question
                 }
             }
         }
